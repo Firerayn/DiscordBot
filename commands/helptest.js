c12308e16c74c6fe55dc9d45a69e7e9567b3d7c4
Firerayn <firerayn@web.de>
2019-05-03 13:04:28 +0200

    Uploaded helptest.js
    Trying my luck on the dynamic help command.

---
+++ commands/helptest.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>{
+  try{
+    await message.delete().catch(O_o ={});
+  }catch(e){
+    let messageembed2 = new Discord.RichEmbed()
+    .setDescription("~Error~")
+    .setColor("#E64100")
+    .addField("Error", "Message could not be deleted.");
+    message.channel.send(messageembed2);
+  }
+//Test 
+const name = args[0].toLowerCase();
+const command = commands.get(name) || commands.find(c => c.name && c.name.includes(name));
+
+if (!command) {
+	return message.reply('that\'s not a valid command!');
+}
+
+data.push(`**Name:** ${command.name}`);
+
+if (command.name) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+if (command.description) data.push(`**Description:** ${command.description}`);
+if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+message.channel.send(data, { split: true });
+/////////
+//Alt    
+/*  let helpembed = new Discord.RichEmbed()
+  .setDescription("~Help~")
+  .setColor("#42e8f4")
+  .addField("!help", "Shows this")
+  .addField("!roles", "Shows all available roles.")
+  .addField("!add", "Sets the specified role. (!add Role)")
+  .addField("!rem", "Removes the specified role. (!rem Role)");
+
+  message.channel.send(helpembed);
+}
+/////////
+*/
+module.exports.help = {
+  name:"helptest",
+  description: "Shows the available commands."
+}
