4f7a09d7295d0bda7b52d7740ffb415efb1aa030
Firerayn <firerayn@web.de>
2019-05-03 10:51:54 +0200

    Add files via upload

--- commands/source.js
+++ commands/source.js
@@ -2,17 +2,21 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) =>{
 
+try{
+    await message.delete().catch(O_o ={});
+  }catch(e){
+    let messageembed2 = new Discord.RichEmbed()
+    .setDescription("~Error~")
+    .setColor("#E64100")
+    .addField("Error", "Message could not be deleted.");
+    message.channel.send(messageembed2);
+  }
+
 const attachment = new Discord.Attachment('https://github.com/Firerayn/DiscordBot/archive/master.zip')
-
-bot.on('message', (receivedMessage) => {
-    if(receivedMessage.author == bot.user) {
-        return
-    }
     
-    receivedMessage.channel.send(attachment)
-})
+    message.channel.send("This is the latest commit of this bots' source.")
+    message.channel.send(attachment)
 
-return;
 }
 
 module.exports.help = {
